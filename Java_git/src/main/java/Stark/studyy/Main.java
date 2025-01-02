0d906ec53e788961d1e18f25f394e5523c8d9249
Stark <1197255051@qq.com>
2025-01-02 22:45:03 +0800

    添加了一个测试页面

--- Java_git/src/main/java/Stark/studyy/Main.java
+++ Java_git/src/main/java/Stark/studyy/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package Stark.studyy;
 
 public class Main {
     public static void main(String[] args) {
---
+++ Java_git/src/main/java/Stark/studyy/testttt.java
@@ -0,0 +1,7 @@
+package Stark.studyy;
+
+public class testttt {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!!");
+    }
+}
